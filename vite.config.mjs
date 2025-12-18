3e894a02b5af2a15fa08f434c251d4123703ffff
Shmuel Zimlichman <shmulikzim@gmail.com>
2025-12-18 22:00:01 +0200

    Fix GitHub Pages build output

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,7 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/pl-stats-react/",
-  server: {
-    port: 5173
+  build: {
+    outDir: "docs",
+    emptyOutDir: true
   }
 });
